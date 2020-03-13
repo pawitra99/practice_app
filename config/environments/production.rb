79f73030a175cab2b95f5c045c9f018eb2804019
Pawitra <pawitra@binus.ac.id>
2020-03-13 13:53:47 +0700

    email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'floating-refuge-10490.herokuapp.com'
+  host = 'practiceapppawitra.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
